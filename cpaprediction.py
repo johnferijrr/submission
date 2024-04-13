a77f2087b320231a9c5b06d6d5986009772ca07b
johnferijrr <112480086+johnferijrr@users.noreply.github.com>
2024-04-13 19:06:04 +0700

    Update cpaprediction.py

--- cpaprediction.py
+++ cpaprediction.py
@@ -120,7 +120,7 @@ st.write("""
 This is a CPA Prediction App that uses machine learning algorithms to predict the Cost Per Acquisition (CPA) for a given set of input features (Cost, CPC, CPM, and CPA) for the 7 days before tomorrow.
 """)
 st.write("""
-Enter the Cost, CPC, CPM, and CPA at day 1 as a Value 1 until Value 4, and so on (don't forget to recheck before click the button!):
+Enter the Cost, CPC, CPM, and CPA at day 1 as a Value 1 until Value 4, and so on (don't forget to recheck again before click the button!):
 """)
 # Create the input widgets for the new name
 new_name_inputs = []
@@ -172,7 +172,7 @@ Please refresh the website if you want input new values
 """)
 # Create the description
 st.write("""
-Enter the CPA at day 1 until day 8 (Tomorrow's CPA Prediction) as CPA 1 until CPA 8 (don't forget to recheck before click the button!):
+Enter the CPA at day 1 until day 8 (Tomorrow's CPA Prediction) as CPA 1 until CPA 8 (don't forget to recheck again before click the button!):
 """)
 
 # Create the input widgets for the new name
