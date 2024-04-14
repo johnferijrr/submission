739b4999a4052949f430c4b6f2bb260d37efb1f3
johnferijrr <112480086+johnferijrr@users.noreply.github.com>
2024-04-14 15:03:01 +0700

    Update cpaprediction.py

--- cpaprediction.py
+++ cpaprediction.py
@@ -202,7 +202,7 @@ Enter the CPA at day 1 until day 8 (Tomorrow's CPA Prediction) as CPA 1 until CP
 new_name_inputs_2 = []
 with st.form("cpa_form_2"):
     for i in range(8):
-        new_name_input = st.text_input(label=f'CPA {i+1}:', key=f'input_{i+1}')
+        new_name_input = st.text_input(label=f'CPA at Day {i+1}:', key=f'input_{i+1}')
         new_name_inputs_2.append(new_name_input)
     if st.form_submit_button("Show Line Chart!"):
         # Get the input values
